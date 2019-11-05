96999ac17cbe5eafb325cef76ddb4084b139a5e5
SergeyOn <seregapantera@gmail.com>
2019-11-05 02:21:16 +0200

    animations

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
+import { ConnectedRouter } from 'connected-react-router';
+import { LastLocationProvider } from "react-router-last-location";
+import configureStore, { history } from 'store';
+import App from 'containers/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import './assets/css/reset.scss';
+import './assets/css/fonts.css';
+import './assets/css/main.css';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const store = configureStore({}, history);
+
+ReactDOM.render(
+<Provider store={store}>
+  <ConnectedRouter history={history}>
+    <LastLocationProvider>
+      <App />
+    </LastLocationProvider>
+  </ConnectedRouter>
+</Provider>
+, document.getElementById('root'));
+
